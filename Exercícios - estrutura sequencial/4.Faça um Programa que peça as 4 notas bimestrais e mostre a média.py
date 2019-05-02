530f5d743720f6735291bacd184fe4739bc0e7ff
matheus8888 <linuxmatheus@outlook.com>
2019-05-02 20:20:14 -0300

    0.1

---
+++ Exercícios - estrutura sequencial/4.Faça um Programa que peça as 4 notas bimestrais e mostre a média.py
@@ -0,0 +1,7 @@
+A = float(input("Digite a Primeira Nota:"))
+B = float(input("Digite a Segunda Nota:"))
+C = float(input("Digite a Terceira Nota:"))
+D = float(input("Digite a Quarta Nota:"))
+media = (A + B + C + D) / 4 
+print("O Resultado Bimestral Foi: ", media)
+
