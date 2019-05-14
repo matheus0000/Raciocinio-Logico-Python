4d7cc6d029b07ffce6a7f34630ef8033fc931b87
matheus8888 <linuxmatheus@outlook.com>
2019-05-14 20:38:17 -0300

    0.1

--- Exercícios - estrutura sequencial/5.Faça um Programa que converta metros para centímetros.py
+++ Exercícios - estrutura sequencial/5.Faça um Programa que converta metros para centímetros.py
@@ -1,3 +1,3 @@
-m = float(input("Digite o Seu Numero em [ metros ] Para Ser Convertido em [ cm ]"))
+m = float(input("Digite o Seu Numero em [ metros ] Para Ser Convertido em [ cm ]: "))
 cm = m * 100;
 print(cm,('cm'))
