cfc43da297049c9ed81a28421dad96ee41810370
matheus8888 <linuxmatheus@outlook.com>
2019-05-30 21:59:30 -0300

    1.0

--- Conversor de Binario Para Decimal-En-Python.py
+++ Conversor de Binario Para Decimal-En-Python.py
@@ -5,8 +5,11 @@ numero = int(input( "Digite o Numero a Ser Convertido Para Binario::" "Seu Numer
 
 time.sleep(2)
 
-valor = numero
-valor = bin(valor)
-print((" Seu Numero Em Binario = "),valor[2:])
+valor0 = numero
+valor1 = numero
+valor0 = hex(valor0)
+valor1 = bin(valor1)
+print((" Seu Numero Em Hexadecimal = "),valor0[2:])
+print((" Seu Numero Em Binario = "),valor1[2:])
 
 time.sleep(5)
